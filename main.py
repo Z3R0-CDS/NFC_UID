ba5e5e94764e52499a7c5b22f040a852c9864112
Z3R0-CDS <78277955+Z3R0-CDS@users.noreply.github.com>
2021-11-09 10:23:28 +0100

    Enhanced experience
    Timeout can be set now.
    Debug can be set for error output
    Better Doc-String

--- main.py
+++ main.py
@@ -6,39 +6,52 @@ from smartcard.Exceptions import CardRequestTimeoutException
 from smartcard.util import toHexString
 import keyboard as Keyboard
 
-class Enviroment():
 
+class Enviroment():
     __last_chip__ = ""
 
-def nfc_reader(output=True, keyboard_output=False):
+def nfc_reader(debug=True ,output=True, keyboard_output=False, set_timeout=120):
     """
     Returns UID of NFC Chip/Card
     Set ouput to False if no output is required default is True
+    debug -> def = True          | Output for errors etc. will be enabled
+    output -> def = True         | Output for success/feedback etc. will be enabled
+    keyboard_output -> def False | Types output like typing it
+    set_timeout -> def 120/2min  | Sets timeout in seconds. Timeout for scan card.
     """
-    try:
-        if output:
-            print("Waiting for Card..")
-        getuid=[0xFF, 0xCA, 0x00, 0x00, 0x00]
-        act = AnyCardType()
-        cr = CardRequest( timeout=10, cardType=act )
-        cs = cr.waitforcard()
-        cs.connection.connect()
-        data, sw1, sw2 = cs.connection.transmit(getuid)
-        data = toHexString(data)
-        if data != "" and data != None and data != Enviroment.__last_chip__:
+    card_not_found = True
+    while card_not_found:
+        try:
             if output:
-                print(f"Success in reading chip..\nUID: {data}")
-            if keyboard_output:
-                Keyboard.write(data)
-            return data
-    except UnboundLocalError:
-        pass
-    except CardRequestTimeoutException:
-        print("Connection timed out... New request starting")
-    except Exception as x:
-        print(f"Error: {x}")
+                print("Waiting for Card..")
+            getuid=[0xFF, 0xCA, 0x00, 0x00, 0x00]
+            act = AnyCardType()
+            cr = CardRequest( timeout=set_timeout, cardType=act )
+            cs = cr.waitforcard()
+            cs.connection.connect()
+            data, sw1, sw2 = cs.connection.transmit(getuid)
+            data = toHexString(data)
+            data = data.replace(" ", "")
+            if data != "" and data != None and data != Enviroment.__last_chip__:
+                card_not_found = False
+                if output:
+                    print(f"Success in reading chip..\nUID: {data}")
+                if keyboard_output:
+                    Keyboard.write(data)
+                return data
+            cs=None
+        except UnboundLocalError:
+            pass
+        except CardRequestTimeoutException:
+            if debug:
+                print("Connection timed out... New request starting")
+        except Exception as x:
+            if debug:
+                print(f"Error: {x}")
     
 
 if __name__ == "__main__":
-    nfc_reader()
+    nfc_reader(True, False)
+
+
 
